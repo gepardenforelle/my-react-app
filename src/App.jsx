01302256c4dc421e6619b04c04a735738d1ee083
m.welisch <m.welisch@gmail.com>
2024-05-06 19:22:31 +0000

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react'
 import './App.css'
 import Product from './components/product'
+import Navbar from './components/navbar'
 
 function App() {
   
   return (
     <div>
+      <Navbar></Navbar>
       <Product/>
     </div>
     
---
+++ src/components/navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+class Navbar extends React.Component {
+
+  state = {}
+  render() {
+
+    return(<nav className="navbar bg-body-tertiary">
+    <div className="container">
+      <a className="navbar-brand" href="#">
+        <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Navbar" width="30" height="24"/>
+      </a>
+    </div>
+  </nav> );
+  }
+}
+
+export default Navbar;
