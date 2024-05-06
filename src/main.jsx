7a557065c2cddd1c4c930e3cb2471ee90cd7b89f
m.welisch <m.welisch@gmail.com>
2024-05-06 20:25:37 +0000

    final 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import ReactDom from 'react-dom'
+import * as ReactDOMClient from 'react-dom/client'
 import '/node_modules/bootstrap/dist/css/bootstrap-grid.min.css'
 import './index.css'
 
 import App from './App.jsx'
 
-const root = ReactDom.createRoot(document.getElementById("root"));
+const root = ReactDOMClient.createRoot(document.getElementById("root"));
 
 root.render(<App/>);
-  
-  
